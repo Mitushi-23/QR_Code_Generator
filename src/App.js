77ea36228d0dfe1bd064c36b6f7a437fc6c224c7
Mitushi-23 <mitushi2agrawal@gmail.com>
2022-06-17 15:31:35 +0530

    added new features

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   Card,
   CardContent,
   Grid,
+  Link,
   TextField,
   Typography,
 } from "@mui/material";
@@ -11,13 +12,16 @@ import "./App.css";
 import QRCode from "qrcode";
 import { useState, useRef } from "react";
 import ClearIcon from "@mui/icons-material/Clear";
-import QrReader from 'react-qr-reader';
+import QrReader from "react-qr-reader";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+// import DoneIcon from '@mui/icons-material/Done';
 
 function App() {
   const [text, setText] = useState("");
   const [imageURL, setImageURL] = useState("");
   const [scanResultFile, setScanResultFile] = useState("");
-  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+  const [scanResultWebCam, setScanResultWebCam] = useState("");
   const qrRef = useRef(null);
   const generateQRCode = async () => {
     try {
@@ -32,9 +36,13 @@ function App() {
     setImageURL("");
   };
 
-  const onScanFile=()=>{
+  const clearQr = () => {
+    setScanResultFile("");
+  };
+
+  const onScanFile = () => {
     qrRef.current.openImageDialog();
-  }
+  };
 
   const handleErrorFile = (error) => {
     console.log(error);
@@ -48,13 +56,23 @@ function App() {
 
   const handleErrorWebCam = (error) => {
     console.log(error);
-  }
+  };
   const handleScanWebCam = (result) => {
-    if (result){
-        setScanResultWebCam(result);
+    if (result) {
+      setScanResultWebCam(result);
     }
-  }
+  };
 
+  // const CopyText = (scanResultWebCam) => {
+
+  //   var text = scanResultWebCam;
+  //   text.select();
+  //   CopyToClipboard(text.value);
+  //   // document.getSelection().removeAllRange();
+  // };
+
+  const str = scanResultFile;
+  const str1 = scanResultWebCam;
   return (
     <>
       <Container style={{ marginTop: "2%" }}>
@@ -63,14 +81,14 @@ function App() {
             variant="h4"
             textAlign="center"
             style={{
-              backgroundColor: "#3f51b5",
-              color: "#fff",
+              backgroundColor: "coral",
+              color: "#000",
               padding: "10px",
             }}
           >
             Generate Download & Scan QR Code
           </Typography>
-          <CardContent>
+          <CardContent style={{ backgroundColor: "beige" }}>
             <Grid container spacing={2}>
               <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
                 <TextField
@@ -79,7 +97,7 @@ function App() {
                   variant="standard"
                   onChange={(e) => setText(e.target.value)}
                 />
-                <ClearIcon onClick={() => clearText()} />
+                <ClearIcon onClick={() => clearText()} style={{cursor:'pointer'}}/>
                 <Button
                   variant="contained"
                   color="primary"
@@ -91,13 +109,19 @@ function App() {
 
                 {imageURL && (
                   <>
-                    <img src={imageURL} alt="img" />
+                    <img
+                      src={imageURL}
+                      alt="img"
+                      width="200px"
+                      style={{ magrin: "auto", textAlign: "center" }}
+                    />
                     <br />
                     <Button
+                      download
+                      style={{ cursor: "pointer", margin: "10px" }}
                       size="small"
                       variant="outlined"
                       href={imageURL}
-                      download
                     >
                       Download
                     </Button>
@@ -105,8 +129,24 @@ function App() {
                 )}
               </Grid>
               <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                <Button variant="contained" color="secondary" onClick={onScanFile}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  style={{ marginTop: "10px", marginBottom: "20px" }}
+                  onClick={onScanFile}
+                >
                   Scan QR Code
+                </Button>
+                <Button
+                  style={{
+                    marginTop: "10px",
+                    marginBottom: "20px",
+                    marginLeft: "10px",
+                  }}
+                  
+                  onClick={() => ( window.location.reload())}
+                >
+                  Clear
                 </Button>
                 <QrReader
                   ref={qrRef}
@@ -116,17 +156,54 @@ function App() {
                   onScan={handleScanFile}
                   legacyMode
                 />
-                <Typography variant="h6">Scanned Code: {scanResultFile}</Typography>
+                <div style={{display:'flex'}}>
+                <Typography variant="h6">Scanned Code:</Typography>
+                <Button>
+                <CopyToClipboard text={scanResultFile}>
+                  <ContentCopyIcon />
+                </CopyToClipboard>
+                </Button>
+                </div>
+                {str.startsWith("http://") || str.startsWith("https://") ? (
+                  <Link href={scanResultFile} target="_blank">{scanResultFile}</Link>
+                ) : (
+                  <Typography>{scanResultFile}</Typography>
+                )}
               </Grid>
               <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                <Typography variant="h6">Qr Code Scan by Web Cam</Typography>
+                <Typography
+                  variant="h6"
+                  style={{ marginTop: "10px", marginBottom: "20px" }}
+                >
+                  Qr Code Scan by Web Cam
+                  <Button
+                    onClick={() => clearQr()}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    Clear
+                  </Button>
+                </Typography>
+
                 <QrReader
-                         delay={300}
-                         style={{width: '100%'}}
-                         onError={handleErrorWebCam}
-                         onScan={handleScanWebCam}
-                         />
-                         <Typography variant="h6">Scanned By WebCam Code: {scanResultWebCam}</Typography>
+                  delay={300}
+                  style={{ width: "100%" }}
+                  onError={handleErrorWebCam}
+                  onScan={handleScanWebCam}
+                />
+                <br />
+                <div style={{display:'flex'}}>
+                <Typography variant="h6">Scanned By WebCam Code:</Typography>
+                <Button >
+                <CopyToClipboard text={scanResultWebCam}>
+                  <ContentCopyIcon />
+                </CopyToClipboard>
+                </Button>
+                </div>
+                {str1.startsWith("http://") ? (
+                  <Link href={scanResultWebCam}target="_blank">{scanResultWebCam}</Link>
+                ) : (
+                  <Typography>{scanResultWebCam}</Typography>
+                )}
               </Grid>
             </Grid>
           </CardContent>
